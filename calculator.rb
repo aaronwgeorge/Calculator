fc3251849c38362f1c043370269d612298edb632
Aaron George <aaronwgeorge@gmail.com>
2014-11-07 11:42:52 -0800

    Pushing calculator app

---
+++ calculator.rb
@@ -0,0 +1,35 @@
+def say(str)
+  puts ".........#{str}........."
+end
+
+say "Enter the first number"
+
+num1 = gets.chomp
+
+say "Enter the second number"
+
+num2 = gets.chomp
+
+say "Press 'A' to Add, 'S' to Subtract, 'M' to Multiply, or 'D' to Divide:"
+
+operator = gets.chomp.upcase
+
+op =
+  case operator
+  when 'A' then '+'
+  when 'S' then '-'
+  when 'M' then '*'
+  when 'D' then '/'
+  end
+
+result = 
+  case operator
+  when 'A' then num1.to_i + num2.to_i
+  when 'S' then num1.to_i - num2.to_i
+  when 'M' then num1.to_i * num2.to_i
+  when 'D' then num1.to_f / num2.to_f 
+  else puts "Invalid Operator"
+  return
+  end
+
+puts "#{num1} #{op} #{num2} = #{result}"
